d665179db6a769dee3f496370752bbaa149d3771
Lily Chisholm <lsc112003@gmail.com>
2020-12-12 16:44:13 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,7 +1,8 @@
 function basicTeenager(age) {
 if (13<=age<=19)
-console.log("You are a teenager!")
-} else {console.log("You are not a teenager")
+return You are a teenager!
+} else {
+return You are not a teenager.
 }
 
 function teenager(age) {
