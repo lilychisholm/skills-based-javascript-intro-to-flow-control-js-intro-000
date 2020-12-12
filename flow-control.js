2b13d2ce47df14a23dda9a313bef2aa9253f1bc1
Lily Chisholm <lsc112003@gmail.com>
2020-12-12 19:15:25 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -26,5 +26,26 @@ return "You are not a teenager"
 }
 
 function switchAge(age) {
-
+case "13" :
+console.log("You are a teenager");
+break;
+case "14" :
+console.log("You are a teenager");
+break;
+case "15" :
+console.log("You are a teenager");
+break;
+case "16" :
+console.log("You are a teenager");
+break;
+case "17" :
+console.log("You are a teenager");
+break;
+case "18" :
+console.log("You are a teenager");
+break;
+case "19" :
+console.log("You are a teenager");
+break;
+return "You have an age"
 }
